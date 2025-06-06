91ff2bbc630eee057cd214a7eeb21f139ee69a08
larissa-felixl <larissa.lima145@aluno.ce.gov.br>
2025-06-06 09:14:29 -0300

    alterações na home

--- home.php
+++ home.php
@@ -8,9 +8,29 @@ include_once'protect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
+    <link rel="stylesheet" href="styles/home.css">
 </head>
 <body>
-    <h1>Bem-vindo, usuário <?= $_SESSION['EMAIL_user'] ?>!</h1>
-    <a href="logout.php">Sair</a>
+    <header id="cabecalho">
+        <div id="logo">
+            <h1 >PlantAi</h1>
+        </div>
+    </header>
+
+    <div id="navegador"> 
+            <a href="index.php">Login</a>
+    </div>
+    
+
+    <div id="fundo">
+        <div></div>
+        <div></div>
+        <h1>Bem-vindo, usuário <?= $_SESSION['EMAIL_user'] ?>!</h1>
+        <a href="logout.php">Sair</a>
+    </div>
+
+    <footer>
+
+    </footer>
 </body>
 </html>
