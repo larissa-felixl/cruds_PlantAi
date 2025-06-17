3ad144d43232defacb0859d9b5c3d60c301ab17e
larissa-felixl <larissa.lima145@aluno.ce.gov.br>
2025-06-17 08:26:36 -0300

    Cadastro das plantas.

--- cadastro_planta.php
+++ cadastro_planta.php
@@ -9,17 +9,21 @@ $stmt = $conexao->prepare("SELECT ID, NAME, DESCRIPTION, IMG FROM CATEGORY WHERE
 $stmt->bindParam(':user_id', $user_id);
 $stmt->execute();
 $categoria = $stmt->fetchall(PDO::FETCH_ASSOC);
+$id_category = $categoria [0]['ID'];
 
 if (isset($_POST['name']) || isset($_POST['description']) || isset($_POST['image'])) {
     if (strlen($_POST['name']) == 0) {
-        $mensagem = "Preencha o campo nome da categoria!";
+        $mensagem = "Preencha o campo nome da planta!";
     } else if (strlen($_POST['description']) == 0) {
-        $mensagem = "Preencha o campo descrição da categoria!";
-    } else if (($_FILES['image']) == 0) {
-        $mensagem = "Preencha o campo imagem da categoria!";
+        $mensagem = "Preencha o campo descrição da planta!";
+    } else if (strlen($_POST['price']) == 0) {
+        $mensagem = "Preencha o campo preço da planta!";
+    }else if (($_FILES['image']) == 0) {
+        $mensagem = "Preencha o campo imagem da planta!";
     }else {
         $name = ($_POST['name']);
         $description = ($_POST['description']);
+        $price = ($_POST['price']);
         $upload_dir = "assets/images/imgs_planta";
         
         if (!is_dir($upload_dir)) {
@@ -34,28 +38,29 @@ if (isset($_POST['name']) || isset($_POST['description']) || isset($_POST['image
         if (!in_array($ext, $ext_permitida)) {
             $mensagem = "Formato de imagem inválido. Use JPG, PNG, JPEG ou WEBP.";
         } else {
-            $novo_nome = uniqid("categoria_", true) . "." . $ext;
+            $novo_nome = uniqid("planta_", true) . "." . $ext;
             $caminho_final = $upload_dir . '/' . $novo_nome;
 
             if (move_uploaded_file($caminho_tmp, $caminho_final)){
                 try {
-                    $vrf = $conexao->prepare("SELECT * FROM CATEGORY WHERE NAME = :name");
+                    $vrf = $conexao->prepare("SELECT * FROM PLANT WHERE NAME = :name");
                     $vrf->bindParam(':name', $name);
                     $vrf->execute();
 
                     if($vrf->rowcount() > 0){
-                        $mensagem = ' Categoria já cadastrada <a href="categoria.php"  id="link_login">Clique aqui para voltar para a página de categorias.</a>';    
+                        $mensagem = ' Categoria já cadastrada <a href="plantas.php" id="link_login">Clique aqui para voltar para a página de plantas.</a>';    
                     
                     } else{
-                        $user_id = $_SESSION['ID_user'];
-                        $stmt = $conexao->prepare("INSERT INTO CATEGORY ( NAME, DESCRIPTION, IMG, USER_ID ) VALUES(:name, :description, :image, :user_id)");
+                        $stmt = $conexao->prepare("INSERT INTO PLANT ( NAME, DESCRIPTION, PRICE, IMG, USER_ID, CATEGORY_ID ) VALUES(:name, :description, :price, :image, :user_id, :category_id)");
                         $stmt->bindParam(':name', $name);
                         $stmt->bindParam(':description', $description);
+                        $stmt->bindParam(':price', $price);
                         $stmt->bindParam(':image', $caminho_final);
                         $stmt->bindParam(':user_id', $user_id);
+                        $stmt->bindParam(':category_id', $id_category);
                         if ($stmt->execute()) {
                             if ($stmt->rowCount() > 0) {                            
-                            header('Location: categoria.php');
+                            header('Location: plantas.php');
                                 exit();
                         } else {
                             $mensagem = "Erro ao tentar efetivar cadastro de categoria.";
@@ -83,7 +88,7 @@ if (isset($_POST['name']) || isset($_POST['description']) || isset($_POST['image
 <body>
     <div id="box">
         <form action="" method="POST" enctype="multipart/form-data">
-            <h1 id="titulo">Descreva sua categoria <?=$categoria[0] ['NAME']?></h1>
+            <h1 id="titulo">Descreva sua planta</h1>
 
             <?php if(!empty($mensagem)): ?>
             <p id="mensagem"> <?= $mensagem ?>  </p>
@@ -95,6 +100,9 @@ if (isset($_POST['name']) || isset($_POST['description']) || isset($_POST['image
             <label for="description">Descrição:</label>
             <input type="text" id="description"  name="description">
 
+            <label for="price">Preço:</label>
+            <input type="decimal" id="price"  name="price">
+
             <label for="image">Fazer upload de imagem ilustrativa:</label>
             <div id="upload-area" onclick="document.getElementById('image').click();">
                 <p>Fazer upload de arquivo .png ou .jpeg</p>
@@ -105,6 +113,5 @@ if (isset($_POST['name']) || isset($_POST['description']) || isset($_POST['image
             <button type="submit">Cadastrar</button>
         </form>
     </div>
-    
 </body>
 </html>
